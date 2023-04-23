134ceeda05269cd4bbf822e8429eb114e0d74cf0
Rafael Freitas <rafael@ezsolucoes.com.br>
2023-04-23 17:05:10 -0300

    Jinja templates

---
+++ exemplos/day1/blog_jinja/wsgi.py
@@ -0,0 +1,83 @@
+import cgi
+from database import conn
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader("templates"))
+
+
+def get_posts_from_database(post_id=None):
+    cursor = conn.cursor()
+    fields = ("id", "title", "content", "author")
+
+    if post_id:
+        results = cursor.execute("SELECT * FROM post WHERE id = ?;", post_id)
+    else:
+        results = cursor.execute("SELECT * FROM post;")
+
+    return [dict(zip(fields, post)) for post in results]
+
+
+def render_template(template_name, **context):
+    template = env.get_template(template_name)
+    return template.render(**context).encode("UTF-8")
+
+
+def add_new_post(post):
+    cursor = conn.cursor()
+    cursor.execute(
+        """\
+            INSERT INTO post (title, content, author)
+            VALUES (:title, :content, :author)
+        """,
+        post
+    )
+    conn.commit()
+
+
+def application(environ, start_response):
+    body = b"Content Not Found"
+    status = "404 Not Found"
+
+    # Processar request
+    path = environ["PATH_INFO"]
+    method = environ["REQUEST_METHOD"]
+
+    if path == "/" and method == "GET":
+        posts = get_posts_from_database()
+        body = render_template(
+            "list.template.html",
+            post_list=posts
+        )
+        status = "200 OK"
+    elif path.split("/")[-1].isdigit() and method == "GET":
+        post_id = path.split("/")[-1]
+        body = render_template(
+            "post.template.html",
+            post=get_posts_from_database(post_id=post_id)[0]
+        )
+        status = "200 OK"
+    elif path == "/new" and method == "GET":
+        body = render_template("form.template.html")
+        status = "200 OK"
+    elif path == "/new" and method == "POST":
+        form = cgi.FieldStorage(
+            fp=environ["wsgi.input"],
+            environ=environ,
+            keep_blank_values=1
+        )
+        post = {item.name: item.value for item in form.list}
+        add_new_post(post)
+        body = b"New post created with success!"
+        status = "201 Created"
+
+    # Criar o response
+    headers = [("Content-type", "text/html")]
+    start_response(status, headers)
+    return [body]
+
+
+if __name__ == "__main__":
+    from wsgiref.simple_server import make_server
+    server = make_server("0.0.0.0", 8000, application)
+    server.serve_forever()
